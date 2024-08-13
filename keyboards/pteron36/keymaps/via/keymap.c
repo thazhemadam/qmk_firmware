0eb318c85ef3b0d6a1760ba3570eae601252e62d
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2024-08-13 09:39:24 +0530

    pteron36: specify layouts for all all possible configuratons of thumbs

--- keyboards/pteron36/keymaps/via/keymap.c
+++ keyboards/pteron36/keymaps/via/keymap.c
@@ -16,6 +16,8 @@
   */
 #include QMK_KEYBOARD_H
 
+#define LAYOUT_split_3x5_3_encoder LAYOUT_split_3x5_3_outer_encoder
+
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
