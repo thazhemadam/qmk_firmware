1711c7a356ff8d4a0b603268cc67feafc81c900f
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2024-11-26 09:52:28 +0530

    feat: remap GPIO

--- keyboards/bastardkb/charybdis/3x5/config.h
+++ keyboards/bastardkb/charybdis/3x5/config.h
@@ -26,7 +26,7 @@
 
 // To use the handedness pin, resistors need to be installed on the adapter PCB.
 // If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP15
+// #define SPLIT_HAND_PIN GP14
 // #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
 
 /* VBUS detection. */
